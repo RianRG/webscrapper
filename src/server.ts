78caf2a4ed258a99a466a678153c3bf84d5a79cd
rian <rianrgomes@gmail.com>
2024-05-22 19:17:04 -0300

    feat: screenshot the page

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('runnin') ;
+import puppeteer from "puppeteer";
+const url = process.env.URL || '';
+const main = async () =>{
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  await page.screenshot({ path: 'test.png' })
+
+  await browser.close();
+}
+
+
+main();
