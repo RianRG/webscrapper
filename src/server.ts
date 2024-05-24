fc2a94720c1f1f2c3a6bb642c846aebd88209364
rian <rianrgomes@gmail.com>
2024-05-24 16:34:32 -0300

    feat: list headphones on index.html

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,26 @@
 import puppeteer from "puppeteer";
 const url = process.env.URL || '';
-
+import fs from 'fs';
 
 const main = async () =>{
-  const browser = await puppeteer.launch({
-    headless: true
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   await page.screenshot({ path: 'test.png' })
-  console.log('passed')
 
   const items = await page.evaluate(() =>{
 
     const items = Array.from(document.querySelectorAll('.compra'))
-    return items.map((item: any) =>{
-      return {
-        title:  item.querySelector('.texto h3')?.textContent,
-        details: item.querySelector('.texto p')?.textContent.replaceAll('\n', '').replaceAll('        ', '')
-      }
-    })
+    return items.map((item: any) =>({
+      title:  item.querySelector('.texto h3')?.textContent,
+      details: item.querySelector('.texto p')?.textContent.replaceAll('\n', '').replaceAll('        ', '')
+    }))
   })
   await browser.close();
+  fs.writeFile('products.json', JSON.stringify(items, null, 2), err =>{
+    if(err) throw new Error('An error occurred!')
+  })
   console.log(items);
 }
 
